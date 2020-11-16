d8938cb7735a5e78b93067e621869d3526fc91d5
shota <shotakannari0607@gmail.com>
2020-11-16 23:32:48 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from blog.models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
